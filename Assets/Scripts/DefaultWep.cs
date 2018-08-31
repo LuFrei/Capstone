b6fbfa047c5539182ef27e4c5a5b3ea7afa856ab
LuFrei <32626445+LuFrei@users.noreply.github.com>
2018-08-31 11:19:48 -0400

    idk, i just want to sync versions

---
+++ Assets/Scripts/DefaultWep.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultWep : MonoBehaviour {
+
+    public PlayerController playerController;
+    public bool fire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( )
+	}
+}
