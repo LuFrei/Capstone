c657033c0ce7f28eb9383e728b04e5af38f3af4e
Lucas Freitas <lucasfreitas09876@gmail.com>
2018-10-24 13:02:12 -0400

    Main menu up and base functionality working

---
+++ Assets/Scripts/GamePlay/Enemies/CannonBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+
+    public Animator anim;
+    public bool hitGround;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        hitGround = true;
+        anim.SetBool("hitGround", hitGround);
+    }
+
+    
+
+}
---
+++ Assets/Scripts/GamePlay/Player/PlayerAnimManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimManager : MonoBehaviour {
+
+    //public Transform playerRender;
+    public Animator TopAnim;
+    public Animator BottomAnim;
+    public Player player;
+  
+    public bool isSprinting;
+    public bool isCrouched;
+    public bool isSliding;
+
+    public bool isJumping;
+    public bool isShooting;
+
+    public bool isLookingUp;
+    public bool isLookingStraight;
+    public bool isLookingDown;
+
+    public bool isFacingLeft;
+
+    public bool isMoving;
+
+    // Use this for initialization
+    void Start () {
+        player = GetComponentInParent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        //Link Input to parameters
+		if(Input.GetAxis("Horizontal") != 0) {
+            isMoving = true;
+        } else {
+            isMoving = false;
+        }
+
+        if(Input.GetAxis("Horizontal") < 0) {
+            isFacingLeft = true;
+        } else if (Input.GetAxis("Horizontal") > 0) {
+            isFacingLeft = false;
+        }
+
+        if(isMoving && Input.GetKey(KeyCode.LeftShift)) {
+            isSprinting = true;
+        } else {
+            isSprinting = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            isLookingUp = true;
+        } else if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            isLookingUp = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !player.isGrounded) {
+            isLookingDown = true;
+        } else if (Input.GetKeyUp(KeyCode.DownArrow) || player.isGrounded) {
+            isLookingDown = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && player.isGrounded) {
+            isCrouched = true;
+        } else if (Input.GetKeyUp(KeyCode.DownArrow) || !player.isGrounded){
+            isCrouched = false;
+        }
+
+        if (!isLookingDown && !isLookingUp) {
+            isLookingStraight = true;
+        } else {
+            isLookingStraight = false;
+        }
+
+
+        if (isFacingLeft) {
+            transform.localScale = new Vector2(-1, 1);
+        } else if (!isFacingLeft){
+            transform.localScale = new Vector2(1, 1);
+        }
+
+
+
+        //Update Animator Values
+        BottomAnim.SetBool("isMoving", isMoving);
+
+    }
+}
