04f196149d5e02f691311d27e5ca32ac6e81d7e6
Lucas Freitas <lucasfreitas09876@gmail.com>
2019-11-25 11:36:21 -0500

    Transfering to Desktop
    Note: After getting a good playable version, push to master, then make a branch of changes

---
+++ Assets/Scripts/Object Behaviors/DoorSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour
+{
+
+
+	//LEGACY SPAGHETTI - These scripts are to be rewrittenand/or thown out!
+	//Like AcidSwitch, This will be handled by a function in 'Switch'
+	public Switch dSwitch;  //switch this class will use
+	public DoorBehavior door;
+
+
+
+	private void Update() {
+		//When reference switch is turned on, open door.
+		if (dSwitch.on) {
+			StartCoroutine(door.Open());
+		}
+	}
+}
---
+++ Assets/Scripts/Object Behaviors/Emmiter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emmiter : MonoBehaviour {
+
+    public GameObject asset;
+    public float rateOfSpawn;
+
+	private float timer;
+
+	public bool moveObject;
+	public float speed;
+	public enum Direction { Up, Down, Left, Right};
+	public Direction direction;
+
+	private void Start() {
+		SetTransform();
+		if (moveObject) {
+			SetMovement();
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0) {
+            Instantiate(asset);
+            timer = rateOfSpawn;
+        }
+	}
+
+
+
+	//This method sets asset to be spawned's transform that to the transform of the emmiter object
+	void SetTransform() {
+		//Set asset Transform
+		asset.transform.position = this.transform.position;		//position
+		asset.transform.rotation = this.transform.rotation;		//rotation
+		asset.transform.localScale = this.transform.localScale; //scale
+	}
+
+	void SetMovement() {
+		asset.GetComponent<Move>().speed = speed;
+		asset.GetComponent<Move>().direction = (Move.Direction)direction;
+	}
+} 
